7cf35e4221fdee8b6d36ea4cfe3d51bc3fb51b18
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-06-11 20:53:00 +0100

    Test and implement note list view

---
+++ src/note-list-view.js
@@ -0,0 +1,21 @@
+ (function(exports) {  
+   function NoteListView(noteList) {
+        this.noteList = noteList;
+    };
+
+    NoteListView.prototype.createHtml = function() {
+        var htmlString = "<ul>";
+
+        for (var i = 0; i < this.noteList.list().length; i++) {
+            htmlString += "<li><div>" 
+                       + this.noteList.list()[i].getNote() 
+                       + "</div></li>";
+        };
+
+        htmlString += "</ul>"
+
+        return htmlString;
+    };
+
+    exports.NoteListView = NoteListView;
+})(this);
